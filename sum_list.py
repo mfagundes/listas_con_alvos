9a91bbc65180ef5b46b43e827d960f03dd8594c8
Mauricio Fagundes <mauricio.fagundes@gmail.com>
2021-09-18 22:58:24 -0300

    listas com valor alvo

---
+++ sum_list.py
@@ -0,0 +1,28 @@
+def calculate_list(numbers_list=None, soma_alvo=10):
+    """
+    Usei coisa básicas, acho que existem soluções bem melhores, especialmente em functools
+    """
+    if numbers_list is None:  # apesar de o enunciado afirmar que a lista não é vazia, eu a testo
+        numbers_list = []
+
+    """assumimos que numbers_list é uma lista de inteiros - aqui eu não validei"""
+    results = []  # uma lista de resultados, para evitar diplicidades (assim entendi no enunciado)
+    if len(numbers_list) <= 1:  # se a lista não tiver um par de inteiros, retorna vazia
+        return results
+
+    # Detesto usar for encadeados, acho sempre que existe uma solução melhor. Mas preferi ficar o básico mesmo
+    for i in range(len(numbers_list)-1):  #itera a lista, com exceção do últio elemento
+        for j in range(len(numbers_list)):  # itera a lista toda (aqui está o encadeamento que não gostei
+            if i == j:  # o enunciado proíbe somar números iguais, então ignoramo-os
+                continue
+            cur_list = [numbers_list[i], numbers_list[j]]  # a lista corrente, que terá os dois valores oriundos dos dois for, já citados antes
+
+            # uma vez que a ordem dos fatores é intiferente, optei por ordená-las, antes de fazer o teste de se já estão
+            # presentes no resultado final e se a soma é igual à soma-alvo.
+            # Cada par estará numa lista que será anexada a uma lista de resultados
+            # Obs: ignorei a premissa de que haverá apenas uma dupla que terá a soma alvo, já que eu evitei a duplicitade.
+            if sorted(cur_list) not in results and sum(cur_list) == soma_alvo:
+                cur_list = sorted(cur_list)
+                results.append(cur_list)
+
+    return results
---
+++ test_results.py
@@ -0,0 +1,32 @@
+import pytest
+
+from sum_list import calculate_list
+
+"""
+Escreva uma função que recebe um array nao vazio de inteiros distintos e um inteiro representando uma soma alvo. 
+Se quaisquer dois números no array de entrada somam a soma alvo, a função deve retornar-los no array de saida em 
+qualquer ordem. Se nenhum par de numeros no array de entrada somarem a soma alvo, a função deve retornar a um 
+array vazio. Note que a soma alvo deve ser obtida somando dois inteiros diferentes do array, você não pode somar 
+um inteiro com ele mesmo.Você pode assumir que existira no maximo um par de números somando a soma alvo.
+
+Exemplo de entrada
+Array [3,5, -4, 8, 11, 1, -1, 6,]
+TargetSum = 10
+
+Exemplo de saida
+[-1, 11] // pode ser na ordem invertida
+"""
+
+
+@pytest.mark.parametrize('numbers_list, soma_alvo, result', (
+        ([], 8, []),
+        ([1, 2], 4, []),
+        ([1, 2, 3, 8, 7], 10, [[2, 8], [3, 7]]),
+        ([1, 2, 3, 9], 5, [[2, 3]]),
+        ([1, 3, 6, 14], 20, [[6, 14]]),
+        ([1, 4, -5, 12, -8, -2], 10, [[-2, 12]]),
+))
+def test_list_sum(numbers_list, soma_alvo, result):
+    res = calculate_list(numbers_list, soma_alvo)
+    assert res == result
+
